7b01773c314917c7c9f9a31737d68036df37c6fe
Peter Bingham <petersbingham@hotmail.co.uk>
2018-03-17 20:45:30 +0000

    Add chart module.

--- ResKit/__init__.py
+++ ResKit/__init__.py
@@ -1,14 +1,42 @@
 import os
 import sys
 fileDir = os.path.dirname(os.path.realpath(__file__))
+modPath = fileDir+'/modules' # Keep modules before dependencies
+sys.path.insert(0,modPath)
 depPath = fileDir+'/dependencies'
 sys.path.insert(0,depPath)
 
+# Only use this to get the defaults and enums (can't override):
+import pynumwrap as nw
+from channelutil.units import *
+
 dependencyOverride = False
 def overrideDependencies():
-  global dependencyOverride
-  if not dependencyOverride:
-    sys.path.remove(depPath)
-    sys.path.append(depPath)
-    dependencyOverride = True
+    global dependencyOverride
+    if not dependencyOverride:
+        sys.path.remove(depPath)
+        sys.path.append(depPath)
+        dependencyOverride = True
 
+typeMode = nw.mode_python
+typeDps = nw.dps_default_python
+def usePythonTypes(dps=nw.dps_default_python):
+    global typeMode
+    global typeDps
+    nw.usePythonTypes(dps)
+    typeMode = nw.mode_python
+    typeDps = dps
+def useMpmathTypes(dps=nw.dps_default_mpmath):
+    global typeMode
+    global typeDps
+    nw.useMpmathTypes(dps)
+    typeMode = nw.mode_python
+    typeDps = dps
+
+MOD_CHART = 0
+# Remove dMat since may want to use several??
+def getModule(modID, parmaFilePaths=None, resultsRoot=None):
+    if modID == MOD_CHART:
+        import chart
+        chart.dMat = dMat
+        return chart
