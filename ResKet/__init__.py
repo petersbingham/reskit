616d67cf8e40e186204d9f91aac9408bac23f744
Peter Bingham <petersbingham@hotmail.co.uk>
2018-03-17 21:42:02 +0000

    Revert previous commit moving files up a level. Remove type set functionality as it can be determined from the containers.

--- ResKet/__init__.py
+++ ResKet/__init__.py
@@ -6,10 +6,6 @@ sys.path.insert(0,modPath)
 depPath = fileDir+'/dependencies'
 sys.path.insert(0,depPath)
 
-# Only use this to get the defaults and enums (can't override):
-import pynumwrap as nw
-from channelutil.units import *
-
 dependencyOverride = False
 def overrideDependencies():
     global dependencyOverride
@@ -18,23 +14,7 @@ def overrideDependencies():
         sys.path.append(depPath)
         dependencyOverride = True
 
-typeMode = nw.mode_python
-typeDps = nw.dps_default_python
-def usePythonTypes(dps=nw.dps_default_python):
-    global typeMode
-    global typeDps
-    nw.usePythonTypes(dps)
-    typeMode = nw.mode_python
-    typeDps = dps
-def useMpmathTypes(dps=nw.dps_default_mpmath):
-    global typeMode
-    global typeDps
-    nw.useMpmathTypes(dps)
-    typeMode = nw.mode_python
-    typeDps = dps
-
 MOD_CHART = 0
-# Remove dMat since may want to use several??
 def getModule(modID, parmaFilePaths=None, resultsRoot=None):
     if modID == MOD_CHART:
         import chart
