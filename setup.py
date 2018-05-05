cf673e968a0b37305c2249d389ca519a975edaa8
Peter Bingham <petersbingham@hotmail.co.uk>
2018-05-05 15:47:44 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='ResKit',
-      version='0.26',
+      version='0.27',
       description='Python Package to assist with the identification and characterisation of quantum scattering resonances.',
       author="Peter Bingham",
       author_email="petersbingham@hotmail.co.uk",
