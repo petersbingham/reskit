49d863f80266eadfab493261ffced4750381da13
Peter Bingham <petersbingham@hotmail.co.uk>
2018-06-23 00:10:03 +0100

    Add some chart tests for the fit.

---
+++ reskit/tests/test_mcsmatfit_chart.py
@@ -0,0 +1,50 @@
+import os
+import sys
+from base64 import test
+fileDir = os.path.dirname(os.path.realpath(__file__))
+rkPath = fileDir+'/../..'
+sys.path.insert(0,rkPath)
+
+import reskit as rk
+rk.safeMode = False
+import channelutil as cu
+import twochanradialwell as rw
+import pynumwrap as nw
+
+import unittest
+import shutil
+
+TEST_ROOT = "test_mcsmatfit_chart"
+if os.path.isdir(TEST_ROOT):
+    shutil.rmtree(TEST_ROOT)
+
+class parent_test(unittest.TestCase):
+    def find_stable_Smat_poles(self):
+        cal = rk.get_asym_calc(cu.hartrees, [0,0])
+        csmat = rw.get_Smat_fun(1.0,2.0,2.0,cal,1.0)
+        dsmat = csmat.discretise(1.,8.,100)
+        mcsmatfit = rk.get_tool(rk.mcsmatfit, dsmat, archive_root=TEST_ROOT,
+                               silent=True)
+
+        cfin = mcsmatfit.get_elastic_Fin(6)
+        csmat = mcsmatfit.get_elastic_Smat(6)
+        
+        chart = rk.get_tool(rk.chart, cfin, archive_root=TEST_ROOT, silent=True)
+        chart.plot_raw(show=False)
+        chart = rk.get_tool(rk.chart, csmat, archive_root=TEST_ROOT, silent=True)
+        chart.plot_Smatrix(show=False)
+
+class test_numpy(parent_test):
+    def runTest(self):
+        rk.use_python_types()
+        self.find_stable_Smat_poles()
+
+class test_mpmath(parent_test):
+    def runTest(self):
+        rk.use_mpmath_types()
+        self.find_stable_Smat_poles()
+
+if __name__ == "__main__":
+    #Just for debug
+    b = test_mpmath()
+    b.runTest()
